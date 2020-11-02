159a934ddd662fac8f73566334555f6e8dc519fd
tanmay192 <btanmay192@gmail.com>
2020-11-02 23:24:54 +0530

    1) Added modal for login ang signup. 2) Added the homescreen carousel.

---
+++ script/form_expander.js
@@ -0,0 +1,9 @@
+var selector = document.getElementById('im');
+selector.addEventListener('change', function() {
+    if (this.value == "Parent") {
+        
+    }
+    else if (this.value == "Baby Sitter") {
+
+    }
+});
