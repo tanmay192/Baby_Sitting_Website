0d1a741d5fb66a1fe73c0601cd646e0f9f2dc3ae
tanmay192 <btanmay192@gmail.com>
2020-11-06 19:15:18 +0530

    1. Gallery section added.

---
+++ script/gallery_slider.js
@@ -0,0 +1,20 @@
+$('#galleryCarousel').carousel({
+    interval : 3000
+  })
+  
+$('.gallery .carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+      
+    for (var i = 0; i < 3; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
