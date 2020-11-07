d39cd9b0a89daae81c9f9af6dd2d5fda93df645a
tanmay192 <btanmay192@gmail.com>
2020-11-08 03:08:13 +0530

    1. Added the banner section. 2. Completed the About Us page. 3. Completed the Caregivers page. 4. Completed the Rates page. 5. Added the gallery page.

--- script/form_expander.js
+++ script/form_expander.js
@@ -1,9 +1,4 @@
-var selector = document.getElementById('im');
-selector.addEventListener('change', function() {
-    if (this.value == "Parent") {
-        
-    }
-    else if (this.value == "Baby Sitter") {
-
-    }
-});
+/*
+ Funtion to expand different sign up form when user selects either parent or babysitter.
+ Feature coming soon 
+*/
